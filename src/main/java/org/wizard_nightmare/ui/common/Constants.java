d9e6559c6d66c25e610ae215960f0be6d1b2f825
jorjitoNM <jorge.novillo@educa.madrid.org>
2025-02-03 13:49:23 +0100

    añadido funcionalidad moverse entre habitaciones y enfrentamiento con la criatura

---
+++ src/main/java/org/wizard_nightmare/ui/common/Constants.java
@@ -0,0 +1,8 @@
+package org.wizard_nightmare.ui.common;
+
+public class Constants {
+
+    public static final String HABITACION_LUCHA = "images/habitacion_lucha.png";
+
+    private Constants() {}
+}
--- src/main/java/org/wizard_nightmare/ui/controllers/RoomController.java
+++ src/main/java/org/wizard_nightmare/ui/controllers/RoomController.java
@@ -1,22 +1,65 @@
 package org.wizard_nightmare.ui.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import lombok.SneakyThrows;
+import org.wizard_nightmare.App;
+import org.wizard_nightmare.game.character.exceptions.WizardTiredException;
+import org.wizard_nightmare.game.demiurge.Demiurge;
+import org.wizard_nightmare.game.demiurge.exceptions.EndGameException;
+import org.wizard_nightmare.game.demiurge.exceptions.GoHomekException;
+import org.wizard_nightmare.ui.common.Constants;
 
-public class RoomController {
+public class RoomController implements DemiurgeConsumer {
 
+    private Demiurge demiurge;
+    @FXML
+    private ImageView creature;
     @FXML
     private AnchorPane screen;
 
 
+    @SneakyThrows
     public void initialize() {
+        screen.setOnKeyPressed(this::handleArrow);
         try {
-            String imagePath = getClass().getResource("/images/habitacion.png").toExternalForm();
+            String imagePath = getClass().getResource(Constants.HABITACION_LUCHA).toExternalForm();
             screen.setStyle("-fx-background-image: url('" + imagePath + "');" +
                     "-fx-background-size: cover;" +
                     "-fx-background-repeat: no-repeat;");
         } catch (NullPointerException e) {
             System.out.println("Image not found!");
         }
+    }
+
+    @Override
+    public void setDemiurge(Demiurge demiurge) {
+        this.demiurge = demiurge;
+        if (this.demiurge.getDungeonManager().hasCreature())
+            creature.setVisible(true);
+    }
+
+    private void handleArrow(KeyEvent event) throws GoHomekException, WizardTiredException, EndGameException {
+        int selection = -1;
+        if (event.getCode() == KeyCode.UP) {
+            selection = 0;
+        } else if (event.getCode() == KeyCode.DOWN) {
+            selection = 1;
+        } else if (event.getCode() == KeyCode.LEFT) {
+            selection = 2;
+        } else if (event.getCode() == KeyCode.RIGHT) {
+            selection = 3;
+        }
+        if (selection > 0 && selection <= demiurge.getDungeonManager().getNumberOfDoors()) {
+            demiurge.getDungeonManager().openDoor(selection - 1);
+        }
+    }
+
+    public void faceCreature(MouseEvent mouseEvent) {
+        App.cambiarPantalla(demiurge, Constants.HABITACION_LUCHA);
     }
 }
