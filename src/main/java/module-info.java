d2d9de591b7f6f662a23fcbca1ee2a8a21675059
jorjitoNM <jorge.novillo@educa.madrid.org>
2025-02-05 10:14:59 +0100

    eliminada funcionalida con xml, anotaciones, module-info.java, pom ...

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,22 +4,20 @@ module org.wizard_nightmare {
     requires com.google.gson;
     requires static lombok;
     requires java.xml;
-    requires jakarta.xml.bind;
 
 
     opens org.wizard_nightmare to javafx.fxml;
     opens org.wizard_nightmare.ui.controllers to javafx.fxml;
-    opens org.wizard_nightmare.game to javafx.fxml, jakarta.xml.bind;
-    opens org.wizard_nightmare.game.dungeon to javafx.fxml, jakarta.xml.bind;
-    opens org.wizard_nightmare.game.objectContainer to javafx.fxml, jakarta.xml.bind;
-    opens org.wizard_nightmare.game.demiurge to javafx.fxml, jakarta.xml.bind;
-    opens org.wizard_nightmare.game.object to javafx.fxml, jakarta.xml.bind;
-    opens org.wizard_nightmare.game.character to javafx.fxml, jakarta.xml.bind;
-    opens org.wizard_nightmare.game.actions to javafx.fxml, jakarta.xml.bind;
-    opens org.wizard_nightmare.game.conditions to javafx.fxml, jakarta.xml.bind;
-    opens org.wizard_nightmare.game.spell to javafx.fxml, jakarta.xml.bind;
-    opens org.wizard_nightmare.game.spellContainer to javafx.fxml, jakarta.xml.bind;
-    opens org.wizard_nightmare.game.util to javafx.fxml, jakarta.xml.bind;
+    opens org.wizard_nightmare.game to javafx.fxml;
+    opens org.wizard_nightmare.game.dungeon to javafx.fxml;
+    opens org.wizard_nightmare.game.objectContainer to javafx.fxml;
+    opens org.wizard_nightmare.game.demiurge to javafx.fxml;
+    opens org.wizard_nightmare.game.object to javafx.fxml;
+    opens org.wizard_nightmare.game.actions to javafx.fxml;
+    opens org.wizard_nightmare.game.conditions to javafx.fxml;
+    opens org.wizard_nightmare.game.spell to javafx.fxml;
+    opens org.wizard_nightmare.game.spellContainer to javafx.fxml;
+    opens org.wizard_nightmare.game.util to javafx.fxml;
     opens org.wizard_nightmare.data to javafx.fxml;
     opens org.wizard_nightmare.console to javafx.fxml;
     opens org.wizard_nightmare.service to javafx.fxml;
