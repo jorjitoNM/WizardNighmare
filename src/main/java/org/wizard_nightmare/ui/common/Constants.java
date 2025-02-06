3ea3fcf70987b7be46969115108ea628817f1538
DAM2 <aitorgarciamont@gmail.com>
2025-02-06 10:07:45 +0100

    Pantalla Finish

--- src/main/java/org/wizard_nightmare/ui/common/Constants.java
+++ src/main/java/org/wizard_nightmare/ui/common/Constants.java
@@ -9,6 +9,8 @@ public class Constants {
     public static final String ROOM_IMAGE = "/images/habitacion.png";
     public static final String HOME_IMAGE =  "/images/habitacion_mago.png";
     public static final String FACE_CREATURE_IMAGE = "/images/habitacion_lucha.png";
+    public static final String FINISH_IMAGE = "/images/finishMazmorra.jpeg";
+    public static final String START = "/screens/start.fxml";
 
     private Constants() {}
 }
